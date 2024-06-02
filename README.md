16a21980fe40f0eb0787f8d121c7d0357a816122
Wesley de Groot <email@wesleydegroot.nl>
2024-06-02 12:10:00 +0200

    Add support for export, improved display colors

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # OSLogViewer
 
-This SwiftUI view/framework is meant for viewing your apps OS_Log history.
+OSLogViewer is made for viewing your apps OS_Log history, it is a SwiftUI view which can be used in your app to view and export your logs.
+
+[![](https://img.shields.io/endpoint?url=https%3A%2F%2Fswiftpackageindex.com%2Fapi%2Fpackages%2F0xWDG%2FOSLogViewer%2Fbadge%3Ftype%3Dplatforms)](https://swiftpackageindex.com/0xWDG/OSLogViewer)
+[![](https://img.shields.io/endpoint?url=https%3A%2F%2Fswiftpackageindex.com%2Fapi%2Fpackages%2F0xWDG%2FOSLogViewer%2Fbadge%3Ftype%3Dswift-versions)](https://swiftpackageindex.com/0xWDG/OSLogViewer)
+[![Swift Package Manager](https://img.shields.io/badge/SPM-compatible-brightgreen.svg)](https://swift.org/package-manager)
+![License](https://img.shields.io/github/license/0xWDG/OSLogViewer)
+
+_Key features:_
+
+- View your apps OS_Log history
+- Export logs
 
 ## Requirements
 
@@ -35,11 +45,10 @@ import OSLogViewer
 ```swift
 import OSLogViewer
 
-// Default configuration
-// uses your app's bundle identifier as subsystem
-// and shows all logs from the last hour.
-
 NavigationLink {
+    // Default configuration
+    // uses your app's bundle identifier as subsystem
+    // and shows all logs from the last hour.
     OSLogViewer()
 } label: {
     Text("View logs")
@@ -82,6 +91,41 @@ OSLogViewer(
 ## Screenshots
 
 <img src='https://github.com/0xWDG/OSLogViewer/assets/1290461/19acba9f-d369-4c1e-bda5-643c7b87a017' height='500'>
+
+## Export example
+
+```plaintext
+This is the OSLog archive for exampleapp
+Generated on 2/6/2024, 11:53
+Generator https://github.com/0xWDG/OSLogViewer
+
+Info message
+ℹ️ 2/6/2024, 11:53 🏛️ exampleapp ⚙️ nl.wesleydegroot.exampleapp 🌐 myCategory
+
+Error message
+❗ 2/6/2024, 11:53 🏛️ exampleapp ⚙️ nl.wesleydegroot.exampleapp 🌐 myCategory
+
+Error message
+❗ 2/6/2024, 11:53 🏛️ exampleapp ⚙️ nl.wesleydegroot.exampleapp 🌐 myCategory
+
+Critical message
+‼️ 2/6/2024, 11:53 🏛️ exampleapp ⚙️ nl.wesleydegroot.exampleapp 🌐 myCategory
+
+Log message
+🔔 2/6/2024, 11:53 🏛️ exampleapp ⚙️ nl.wesleydegroot.exampleapp 🌐 myCategory
+
+Log message
+🔔 2/6/2024, 11:53 🏛️ exampleapp ⚙️ nl.wesleydegroot.exampleapp 🌐 myCategory
+```
+
+## Changelog
+
+- 1.0.0
+  - Initial release
+- 1.0.1
+  - Improved support for dark mode.
+  - Colors are more similar to Xcode's console.
+  - Added support for exporting logs.
 
 ## Contact
 
--- Sources/OSLogViewer/OSLogViewer.swift
+++ Sources/OSLogViewer/OSLogViewer.swift
@@ -32,24 +32,49 @@ public struct OSLogViewer: View {
     }
 
     @State
+    /// This variable saves the log messages
     private var logMessages: [OSLogEntryLog] = []
 
     @State
+    /// This variable saves the current state
     private var finishedCollecting: Bool = false
 
+    @State
+    /// This variable saves the export sheet state
+    private var exportSheet: Bool = false
+
     public var body: some View {
-        VStack {
+        NavigationView {
             List {
                 ForEach(logMessages, id: \.self) { entry in
                     VStack {
+                        // Actual log message
                         Text(entry.composedMessage)
+
+                        // Details (time, framework, subsystem, category
                         detailsBuilder(for: entry)
                             .font(.footnote)
                     }
                     .listRowBackground(getBackgroundColor(level: entry.level))
                 }
             }
-            .navigationTitle("OSLog viewer")
+            .navigationViewStyle(.stack) // iPad
+            .navigationBarTitle("OSLog viewer", displayMode: .inline)
+            .listStyle(InsetGroupedListStyle())
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    ShareLink(
+                        items: export()
+                    ).disabled(!finishedCollecting)
+                }
+            }
+        }
+        .sheet(isPresented: $exportSheet) {
+            ShareLink(
+                items: export(),
+                subject: Text("OSLog Archive"),
+                message: Text("This is the OSLog archive for this application")
+            )
         }
         .overlay {
             if logMessages.isEmpty {
@@ -88,7 +113,34 @@ public struct OSLogViewer: View {
         }
     }
 
+    func export() -> [String] {
+        let appName: String = {
+            if let displayName: String = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String {
+                return displayName
+            } else if let name: String = Bundle.main.infoDictionary?["CFBundleName"] as? String {
+                return name
+            }
+            return "this application"
+        }()
+
+        return [
+            [
+                "This is the OSLog archive for \(appName).\r\n",
+                "Generated on \(Date().formatted())\r\n",
+                "Generator https://github.com/0xWDG/OSLogViewer\r\n\r\n",
+                logMessages.map {
+                    "\($0.composedMessage)\r\n" +
+                    getLogLevelEmoji(level: $0.level) +
+                    " \($0.date.formatted()) 🏛️ \($0.sender) ⚙️ \($0.subsystem) 🌐 \($0.category)"
+                }.joined(separator: "\r\n\r\n"),
+            ].joined()
+        ]
+    }
+
     @ViewBuilder
+    /// Build details (time, framework, subsystem, category), for the footnote row
+    /// - Parameter entry: log entry
+    /// - Returns: Text containing icons and details.
     func detailsBuilder(for entry: OSLogEntryLog) -> Text {
         // No accebility labels are used,
         // If added it will _always_ file to check in compile time.
@@ -96,65 +148,134 @@ public struct OSLogViewer: View {
         Text(" ") +
         Text(entry.date, style: .time) +
         Text(" ") +
+        // 􀤨 Framework (aka sender)
         Text("\(Image(systemName: "building.columns")) \(entry.sender) ") +
+        // 􀥎 Subsystem
         Text("\(Image(systemName: "gearshape.2")) \(entry.subsystem) ") +
-        Text("\(Image(systemName: "square.stack.3d.up")) \(entry.category)")
+        // 􀦲 Category
+        Text("\(Image(systemName: "square.grid.3x3")) \(entry.category)")
     }
 
+    /// Generate an emoji for the current log level
+    /// - Parameter level: log level
+    /// - Returns: Emoji
+    func getLogLevelEmoji(level: OSLogEntryLog.Level) -> String {
+        switch level {
+        case .undefined, .notice:
+            "🔔"
+        case .debug:
+            "🩺"
+        case .info:
+            "ℹ️"
+        case .error:
+            "❗"
+        case .fault:
+            "‼️"
+        default:
+            "🔔"
+        }
+    }
+
+    /// Generate an icon for the current log level
+    /// - Parameter level: log level
+    /// - Returns: SF Icon as Text
     func getLogLevelIcon(level: OSLogEntryLog.Level) -> Text {
         switch level {
         case .undefined, .notice:
+            // 􀼸
             Text(Image(systemName: "bell.square.fill"))
                 .accessibilityLabel("Notice")
         case .debug:
+            // 􀝾
             Text(Image(systemName: "stethoscope"))
                 .accessibilityLabel("Debug")
         case .info:
+            // 􁊇
             Text(Image(systemName: "info.square"))
                 .accessibilityLabel("Information")
         case .error:
+            // 􀢒
             Text(Image(systemName: "exclamationmark.2"))
                 .accessibilityLabel("Error")
         case .fault:
+            // 􀣴
             Text(Image(systemName: "exclamationmark.3"))
                 .accessibilityLabel("Fault")
         default:
+            // 􀼸
             Text(Image(systemName: "bell.square.fill"))
                 .accessibilityLabel("Default")
         }
     }
-
+    
+    /// Generate the background color for the log message
+    /// - Parameter level: log level
+    /// - Returns: The appropiate color
     func getBackgroundColor(level: OSLogEntryLog.Level) -> Color {
         switch level {
         case .undefined, .debug, .info, .notice:
-            Color.white
+            Color(uiColor: UIColor.secondarySystemGroupedBackground)
+
         case .error:
-            Color.yellow
+            // Fetched colors with color picker from Xcode
+            // Using a `dynamicProvider` to support light & dark mode.
+            Color(uiColor: .init(dynamicProvider: { traits in
+                if traits.userInterfaceStyle == .light {
+                    return .init(red: 1, green: 0.968, blue: 0.898, alpha: 1)
+                } else {
+                    return .init(red: 0.858, green: 0.717, blue: 0.603, alpha: 0.4)
+                }
+            }))
+
         case .fault:
-            Color.red
+            // Fetched colors with color picker from Xcode
+            // Using a `dynamicProvider` to support light & dark mode.
+            Color(uiColor: .init(dynamicProvider: { traits in
+                if traits.userInterfaceStyle == .light {
+                    return .init(red: 0.98, green: 0.90, blue: 0.90, alpha: 1)
+                } else {
+                    return .init(red: 0.26, green: 0.15, blue: 0.17, alpha: 1)
+
+                }
+            }))
+
         default:
-            Color.clear
+            Color(uiColor: UIColor.secondarySystemGroupedBackground)
         }
     }
-
+    
+    /// Get the logs
     public func getLog() async {
+        // We start collecting
         finishedCollecting = false
 
         do {
+            /// Initialize logstore for the current proces
             let logStore = try OSLogStore(scope: .currentProcessIdentifier)
+
+            /// Fetch all logs since a specific date
             let sinceDate = logStore.position(date: since)
+
+            /// Predicate (filter) all results to have the subsystem starting with the given subsystem
             let predicate = NSPredicate(format: "subsystem BEGINSWITH %@", subsystem)
+
+            /// Get all logs from the log store
             let allEntries = try logStore.getEntries(at: sinceDate, matching: predicate)
 
+            /// Remap from `AnySequence<OSLogEntry>` to type `[OSLogEntryLog]`
             logMessages = allEntries.compactMap { $0 as? OSLogEntryLog }
         } catch {
+            // We fail to get the results, add this to the log.
             os_log(.fault, "Something went wrong %@", error as NSError)
         }
 
+        // We've finished collecting
         finishedCollecting = true
     }
 }
 
-#Preview {
-    OSLogViewer()
+struct OSLogViewer_Previews : PreviewProvider {
+    static var previews: some View {
+        OSLogViewer()
+    }
 }
